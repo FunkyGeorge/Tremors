d6cf0d0dd5d954fa24c0579587152e5bfd3dd3ca
George Miranda <george92miranda@gmail.com>
2024-10-09 23:01:37 -0700

    Create a runner puzzle

---
+++ Assets/Scripts/Puzzles/PentaPuzzle.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PentaPuzzle : MonoBehaviour
+{
+
+    enum PuzzleState {
+        Waiting,
+        Solving,
+        Solved
+    }
+    [SerializeField] private Sprite unluckySprite;
+    [SerializeField] private Sprite luckySprite;
+    [SerializeField] private List<PuzzleNode> nodes = new List<PuzzleNode>();
+    private PuzzleState state = PuzzleState.Waiting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < nodes.Count; i++) {
+            nodes[i].SetIndex(i);
+            nodes[i].gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (state == PuzzleState.Waiting) {
+            Runner validRunner = collider.gameObject.GetComponent<Runner>();
+            if (validRunner) {
+                foreach (PuzzleNode node in nodes) {
+                    node.gameObject.SetActive(true);
+                    state = PuzzleState.Solving;
+                }
+            }
+        }
+    }
+
+    public void OnToggleNode(int nodeIndex) {
+        bool isSolved = true;
+        for (int i = 0; i < nodes.Count; i++) {
+            if (i == (0 + nodeIndex) % nodes.Count || i == (2 + nodeIndex) % nodes.Count || i == (3 + nodeIndex) % nodes.Count) {
+                nodes[i].Toggle();
+            }
+            isSolved = isSolved && nodes[i].active;
+        }
+
+        if (isSolved) {
+            SetSolved();
+        }
+    }
+
+    public void SetSolved() {
+        state = PuzzleState.Solved;
+        for (int i = 0; i < nodes.Count; i++) {
+            nodes[i].gameObject.SetActive(false);
+        }
+        GetComponent<SpriteRenderer>().sprite = luckySprite;
+    }
+}
---
+++ Assets/Scripts/Puzzles/PuzzleNode.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleNode : MonoBehaviour
+{
+    public bool active = false;
+    public event EventHandler OnToggle;
+    [SerializeField] private Color activeColor;
+    [SerializeField] private Color inactiveColor;
+    private PentaPuzzle parentPuzzle;
+    private int index = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentPuzzle = transform.parent.GetComponent<PentaPuzzle>();
+        if (!parentPuzzle) {
+            Debug.LogError("Could not find parent puzzle!");
+        }
+        SyncColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetIndex(int i) {
+        index = i;
+    }
+
+    public void Toggle() {
+        active = !active;
+        SyncColor();
+    }
+
+    private void SyncColor() {
+        if (active) {
+            GetComponent<SpriteRenderer>().color = activeColor;
+        } else {
+            GetComponent<SpriteRenderer>().color = inactiveColor;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+            Runner validRunner = collider.gameObject.GetComponent<Runner>();
+            if (validRunner) {
+                parentPuzzle.OnToggleNode(index);
+            }
+    }
+}
